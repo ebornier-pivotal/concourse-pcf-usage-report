8b1949fabef18fbcf9380b0e2914c001171d6d93
Erwan Bornier <ebornier@pivotal.io>
2018-05-26 14:48:29 +0200

    modify pipeline

--- ci/tasks/filter-usage-data.sh
+++ ci/tasks/filter-usage-data.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
 
+cf login -u $SYS_ADMIN_USER -p $SYS_ADMIN_PASSWORD -a $PCF_API_ENDPOINT -o system -s system --skip-ssl-validation
+
+
 ./pipeline-scripts/ci/tasks/filter.sh -trial ./orgs-usage-consolidated/report.json
