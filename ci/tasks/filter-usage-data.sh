6bcd1bb0b7205a5f7666c368197967a5504ce487
Dirk Hoffmann (Dell EMC) <Dirk.Hoffmann@dell.com>
2018-06-27 10:59:34 +0200

    transfer consolidated report to azure blob

--- ci/tasks/filter-usage-data.sh
+++ ci/tasks/filter-usage-data.sh
@@ -3,6 +3,7 @@
 
 cf login -u $SYS_ADMIN_USER -p $SYS_ADMIN_PASSWORD -a $PCF_API_ENDPOINT -o system -s system --skip-ssl-validation
 
+ls -l *
 
 ./pipeline-scripts/ci/tasks/filter.sh -trial ./orgs-usage-consolidated/report.json
 
